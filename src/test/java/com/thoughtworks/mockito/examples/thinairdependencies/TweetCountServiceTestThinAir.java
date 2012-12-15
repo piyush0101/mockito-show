b1473734a8be991d657cc9462c3dd4879bcaa546
Piyush Srivastava <piyush.0101@gmail.com>
2012-12-15 03:15:48 -0600

    removed target folder from source control

---
+++ src/test/java/com/thoughtworks/mockito/examples/thinairdependencies/TweetCountServiceTestThinAir.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.mockito.examples.thinairdependencies;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThoughtWorks
+ * Date: 12/13/12
+ * Time: 3:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TweetCountServiceTestThinAir {
+}
